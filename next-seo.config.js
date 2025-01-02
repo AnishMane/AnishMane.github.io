a56f0b498b0e47c6169571bffc994240334c470e
AnishMane <anishmane70@gmail.com>
2025-01-02 13:49:36 +0530

    feat: added projects to projectsData

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,38 +1,38 @@
-const canonicalUrl = 'https://aulianza.id';
-const metaImage = 'https://cloud.aulianza.com/public/images/aulianza-id.png';
+const canonicalUrl = 'https://anishmane.com';
+const metaImage = '';
 const metaDescription =
   'Seasoned Software Engineer especially in Frontend side, with a passion for creating pixel-perfect web experiences';
 
 const defaultSEOConfig = {
-  defaultTitle: 'Ryan Aulia - Personal Website',
+  defaultTitle: 'Anish Mane',
   description: metaDescription,
   canonical: canonicalUrl,
   openGraph: {
     canonical: canonicalUrl,
-    title: 'Anish Mane - Personal Website',
+    title: 'Anish Mane',
     description: metaDescription,
     type: 'website',
     images: [
       {
         url: metaImage,
-        alt: 'aulianza.id og-image',
+        alt: 'anishmane.com og-image',
         width: 800,
         height: 600,
       },
       {
         url: metaImage,
-        alt: 'aulianza.id og-image',
+        alt: 'anishmane.com og-image',
         width: 1200,
         height: 630,
       },
       {
         url: metaImage,
-        alt: 'aulianza.id og-image',
+        alt: 'anishmane.com og-image',
         width: 1600,
         height: 900,
       },
     ],
-    site_name: 'aulianza.id',
+    site_name: 'anishmane.com',
   },
   twitter: {
     handle: '@handle',
--- src/common/data/projectsData.ts
+++ src/common/data/projectsData.ts
@@ -3,10 +3,10 @@ import { ProjectsProps } from '@/common/types/projects';
 export const hardcodedProjects: ProjectsProps['projects'] = [
   {
     title: 'Dining-Concierge-Chatbot',
-    slug: 'project-one',
+    slug: 'Dining-Concierge-Chatbot',
     description: `This application is a serverless, microservice-driven web application built entirely with Amazon Web Services. This chatbot's main function is to make restaurant recommendations to its customers based on their preferences expressed through conversations.`,
     image: 'https://i.postimg.cc/dVqmTdfF/dcc.jpg',
-    stacks: JSON.stringify(['JavaScript', 'React.js', "Lambda", "Python", ""]),
+    stacks: JSON.stringify(['JavaScript', 'React.js', 'Lambda', 'Python', '']),
     is_featured: true,
     is_show: true,
     link_demo: '',
@@ -167,17 +167,462 @@ We have support for Yelp-API with suggestions and real time chat.
     updated_at: new Date(),
   },
   {
-    title: 'Project Two',
-    slug: 'project-two',
-    description: 'This is the second hardcoded project.',
-    image: '/path/to/image2.jpg',
-    stacks: JSON.stringify(['Next.js', 'Tailwind CSS']),
+    title: 'Cancer-Associated-Biomarker-Identification',
+    slug: 'Cancer-Associated-Biomarker-Identification',
+    description:
+      'This project aims to identify cancer biomarkers using a Python backend (Flask API) and a React frontend, incorporating machine learning capabilities for predictions.',
+    image: 'https://i.postimg.cc/JnvxQ8q3/07-Blog-Cancer-Cells-L.jpg',
+    stacks: JSON.stringify([
+      'React.js',
+      'TailwindCSS',
+      'Python',
+      'Jupyter',
+      'Pandas',
+      'JavaScript',
+    ]),
+    is_featured: true,
+    is_show: true,
+    link_demo: '',
+    link_github:
+      'https://github.com/AnishMane/Cancer-Associated-Biomarker-Identification',
+    content: `
+  ## Introduction
+  
+  This project aims to identify cancer biomarkers using a Python backend (Flask API) and a React frontend, incorporating machine learning capabilities for predictions.
+  
+  ## Table of Contents
+  - [Project Description](#project-description)
+  - [Prerequisites](#prerequisites)
+  - [Setup Instructions](#setup-instructions)
+    - [1. Clone the Repository](#1-clone-the-repository)
+    - [2. Setting Up the Backend (Python and Flask)](#2-setting-up-the-backend-python-and-flask)
+    - [3. Setting Up the Frontend (React)](#3-setting-up-the-frontend-react)
+  
+  ## Project Description
+  
+  This project provides an interface for identifying potential cancer biomarkers. It uses a Python backend powered by Flask, where the machine learning models are hosted, and a React frontend that allows users to interact with the model and view results.
+  
+  ## Prerequisites
+  
+  - **Node.js and npm** (for frontend)
+  - **Python 3 and pip** (for backend)
+  - \`virtualenv\` (recommended for managing Python dependencies)
+  
+  ## Setup Instructions
+  
+  ### 1. Clone the Repository
+  
+  Clone the repository to your local machine and navigate into the project directory:
+  
+  \`\`\`bash
+  git clone https://github.com/AnishMane/Cancer-Associated-Biomarker-Identification.git
+  cd Cancer-Associated-Biomarker-Identification
+  \`\`\`
+  
+  ### 2. Setting Up the Backend (Python and Flask)
+  
+  \`\`\`bash
+  cd backend
+  python -m venv venv   # (or \`python3 -m venv venv\` for Mac)
+  venv/Scripts/activate  # (or \`source venv/bin/activate\` for Mac)
+  pip install -r requirements.txt
+  \`\`\`
+  
+  ### 3. Setting Up the Frontend (React)
+  
+  \`\`\`bash
+  cd frontend
+  npm install
+  npm start
+  \`\`\`
+    `,
+    updated_at: new Date(),
+  },
+  {
+    title: 'Duplexa',
+    slug: 'Duplexa',
+    description:
+      'Duplexa, powered by React, combines a Python backend and React frontend to streamline data redundancy removal. This web app optimizes storage, enhances data integrity, and provides a user-friendly interface for efficient data management. Simplify redundancy challenges with Duplexa.',
+    image:
+      'https://i.postimg.cc/Gt9kV5ML/business-intelligence-data-redundancy.png',
+    stacks: JSON.stringify([
+      'React.js',
+      'Python',
+      'Pandas',
+      'Numpy',
+      'Jupyter',
+    ]),
+    is_featured: true,
+    is_show: true,
+    link_demo: '',
+    link_github: 'https://github.com/AnishMane/Duplexa',
+    content: `
+  ## 🚀 Duplexa (Data Redundancy Removal Algorithm)
+  
+  **Description:**  
+  Data Redundancy Removal Algorithm is a project aimed at efficiently removing data redundancy from large datasets. The backend is implemented in Python, utilizing the XXHash algorithm to identify and eliminate redundant data. The frontend is developed using ReactJS, providing users with a user-friendly interface to manage and visualize the data.
+  
+  ### 📄 Our PPT for Bhuvan ISRO Hackathon - [Duplexa.pdf](https://github.com/AnishMane/Duplexa/files/14734767/Duplexa.pdf)
+  
+  ---
+  
+  ## Table of Contents
+  
+  - [Installation](##installation) 🛠️
+  - [Getting Started](#getting-started) 🏁
+  - [Usage](#usage) 💻
+  - [Troubleshooting](#troubleshooting) 🛠️
+  - [Contributing](#contributing) 🤝
+  - [License](#license) 📝
+  - [Acknowledgements](#acknowledgements) 🙏
+  - [Contact](#contact) 📬
+  - [Version History](#version-history) 📅
+  
+  ---
+  
+  ## Installation 🛠️
+  
+  ### Prerequisites
+  - Python 3.x
+  - Node.js
+  
+  ### Installation Steps
+  1. Clone the repository: \`git clone https://github.com/AnishMane/Duplexa.git\`
+  2. Navigate to the project directory: \`cd Duplexa\`
+  3. Install Python dependencies: \`pip install -r requirements.txt\`
+  4. Install Node.js dependencies for the frontend: \`npm install\`
+  
+  ---
+  
+  ## Getting Started 🏁
+  
+  After completing the installation steps, follow these instructions to start using the project:
+  
+  1. Run the Python backend server: \`python app.py\`
+  2. Start the React frontend application: \`npm start\`
+  3. Access the application in your web browser at \`http://localhost:3000\`
+  
+  ---
+  
+  ## Usage 💻
+  
+  To use the Data Redundancy Removal Algorithm:
+  
+  1. Upload your dataset to the application.
+  2. The algorithm will analyze the dataset and remove redundant data using the XXHash algorithm.
+  3. View the optimized dataset and download the processed data if needed.
+  
+  ---
+  
+  ## Troubleshooting 🛠️
+  
+  ### Issue: Backend server fails to start
+  - **Solution:** Ensure that Python 3.x is installed and all dependencies are installed correctly. Check for any error messages in the console for further diagnosis.
+  
+  ### Issue: Frontend application crashes unexpectedly
+  - **Solution:** Make sure Node.js is installed and all frontend dependencies are installed using \`npm install\`. Check the console for any error messages to identify the issue.
+  
+  ---
+  
+  ## Contributing 🤝
+  
+  Contributions are welcome! Please follow these guidelines when contributing to the project:
+  - Fork the repository and create a new branch for your feature or bug fix.
+  - Make your changes and submit a pull request detailing the changes made.
+  - Ensure that your code follows the project's coding standards and conventions.
+  
+  ---
+  
+  ## Contact 📬
+  
+  For any questions, feedback, or support, please contact us at [anishmane70@gmail.com](mailto:anishmane70@gmail.com).
+    `,
+    updated_at: new Date(),
+  },
+  {
+    title: 'Google-Keep-Replica-Project',
+    slug: 'Google-Keep-Replica-Project',
+    description:
+      'Replicate Google Keep using React.js, focusing on dynamic note components, smooth navigation with React Router, and simple state management for a user-friendly experience.',
+    image: 'https://i.postimg.cc/7PcsD3Ss/03t5awzh-DYxo5-Qf3-EUytp21-15.webp',
+    stacks: JSON.stringify(['JavaScript', 'React.js', 'Node.js', 'MongoDB']),
     is_featured: false,
     is_show: true,
-    link_demo: 'https://project-two-demo.com',
-    link_github: 'https://github.com/user/project-two',
+    link_demo: '',
+    link_github: 'https://github.com/AnishMane/Google-Keep-Replica-Project',
+    content: `
+    
+
+
+  [Video Preview](https://github.com/AnishMane/Google-Keep-Replica-Project/assets/112772218/cb68d059-ad98-4411-8198-f439688fea45)
+
+
+
+## Google Keep Replica
+
+**📝 Description:**
+
+
+-> This project is a replica of Google Keep, a note-taking application developed by Google. It provides similar functionalities for creating, editing, and organizing notes in a user-friendly interface.
+
+## 🚀 Features:
+
+
+1. Create Notes: Easily create new notes with titles and content.
+
+2. Edit Notes: Edit existing notes to update information.
+
+3. Organize: Categorize notes with labels or color-coding.
+
+4. Search: Quickly find notes using the search functionality.
+
+5. Reminders: Set reminders for important notes.
+
+6. Responsive Design: Ensures optimal viewing and interaction experience across devices.
+
+## 🛠️ Installation:
+
+-> Clone the repository
+
+-> Navigate to the project directory
+
+-> Install dependencies: npm install or yarn install
+
+-> Start the application: npm start or yarn start
+
+
+## 🔧 Usage:
+
+
+-> Open the application in your browser.
+
+-> Start creating and managing your notes.
+
+
+## 👥 Authors:
+
+
+**AnishMane**
+
+###📧 Contact:
+
+
+-> For any inquiries or support, please contact us at anishmane70@gmail.com.
+
+**🚨 Disclaimer:**
+
+
+-> This project is a replica of Google Keep and is intended for educational purposes only. It is not affiliated with or endorsed by Google.
+
+    `,
+    updated_at: new Date(),
+  },
+  {
+    title: 'UniStorage',
+    slug: 'UniStorage',
+    description:
+      "UniStorage: A student property management system developed at Vellore Institute of Technology's 12-hour hackathon. It simplifies item tracking within campus housing using the MERN stack, prioritizing efficiency and transparency. Designed to scale effortlessly.",
+    image: 'https://i.postimg.cc/WpM8RBb3/student-housin-notebook.png',
+    stacks: JSON.stringify(['JavaScript', 'React.js', 'Node.js', 'MongoDB']),
+    is_featured: false,
+    is_show: true,
+    link_demo: '',
+    link_github: 'https://github.com/AnishMane/UniStorage',
+    content: `
+  ## 🏫 UniStorage: Student Property Management System
+  
+  ## Table of Contents
+  - [Preview](#preview)
+  - [About The Project](#about-the-project)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+  - [Contributing](#contributing)
+  - [Contact](#contact)
+  
+  ## Preview
+  
+  https://github.com/AnishMane/UniStorage/assets/112772218/a88ae0d8-ea94-44bb-97e2-e59eacbee218
+  
+  ## About The Project
+  ### Overview
+  
+  ***UniStorage was developed in a 12-hour hackathon held by Vellore Institute of Technology.***
+  
+  UniStorage is a comprehensive student property management system developed using the MERN stack (MongoDB, Express.js, React, and Node.js). This project aims to streamline the management, tracking, and allocation of student-owned properties such as mattresses, blankets, buckets, and other essential items within a campus or student housing environment. The system is designed to be user-friendly, efficient, and scalable, catering to the needs of both students and administrators.
+  
+  ### Objectives
+  ### 🚀 Efficient Management: 
+  To provide a centralized platform for managing student properties, reducing the administrative burden on staff.
+  ### 🔍 Transparency: 
+  To enhance transparency in the allocation and return of items, ensuring that students and staff can easily track the status of each item.
+  ### 🎨 User-Friendly Interface: 
+  To offer a simple and intuitive interface for both students and administrators, making the process of borrowing and returning items hassle-free.
+  ### ⚖️ Scalability: 
+  To ensure the system can handle a growing number of users and properties without compromising performance.
+  
+  ## Prerequisites
+  This is an example of how to list things you need to use the software and how to install them.
+  - npm
+    \`\`\`sh
+    npm install npm@latest -g
+    \`\`\`
+  
+  ### Installation
+  
+  1. Clone the repo
+     \`\`\`sh
+     git clone https://github.com/AnishMane/UniStorage.git
+     \`\`\`
+  2. Install NPM packages
+     \`\`\`sh
+     npm install
+     \`\`\`
+  3. Start the client using
+     \`\`\`sh
+     npm start
+     \`\`\`
+  4. Start server using
+     \`\`\`sh
+     npm run dev
+     \`\`\`
+  
+  ## Contributing
+  
+  Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+  
+  If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+  Don't forget to give the project a star! Thanks again!
+  
+  1. Fork the Project
+  2. Create your Feature Branch
+  3. Commit your Changes
+  4. Push to the Branch
+  5. Open a Pull Request
+  
+  ## Contact
+  
+  Anish - anishmane70@gmail.com
+    `,
+    updated_at: new Date(),
+  },
+  {
+    title: 'PersonHub',
+    slug: 'PersonHub',
+    description:
+      'PersonHub is a MERN stack web app simplifying personal information management. Easily add, view, update, and delete persons with a user-friendly interface. Ideal for learning MERN stack basics and building upon for more complex projects.',
+    image: 'https://i.postimg.cc/fyGQ01pk/resume-personal-information.webp',
+    stacks: JSON.stringify(['JavaScript', 'React.js', 'Node.js', 'MongoDB']),
+    is_featured: false,
+    is_show: true,
+    link_demo: '',
+    link_github: 'https://github.com/AnishMane/PersonHub',
+    content: `
+## 🧑‍💼 PersonHub
+
+## Table of Contents
+- [About The Project](#about-the-project)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Contributing](#contributing)
+- [Contact](#contact)
+
+## About The Project
+PersonHub is a web application designed to simplify the management of personal information. Built with the MERN stack (MongoDB, Express.js, React.js, Node.js), this project showcases a straightforward implementation of CRUD (Create, Read, Update, Delete) operations.
+
+### Key Features
+- **Add New Persons:** Easily add individuals by providing their name, email, and age.
+- **View All Persons:** Display a list of all entered persons in an organized manner.
+- **Update Details:** Modify the information of any person with ease.
+- **Delete Persons:** Remove any person from the list with a single click.
+
+### Purpose
+The primary goal of PersonHub is to provide a practical example of a full-stack application using the MERN stack. It demonstrates how to connect a React frontend to a Node.js and Express backend, and how to perform CRUD operations on a MongoDB database.
+
+#### Why PersonHub?
+- **Learning Resource:** Ideal for developers who are new to the MERN stack and want to see a complete application in action.
+- **Reusable Codebase:** Provides a solid foundation that can be expanded and customized for more complex projects.
+- **User-Friendly:** Focuses on simplicity and ease of use, ensuring that even those with minimal technical background can navigate and use the app effectively.
+
+PersonHub exemplifies how modern web development technologies can be leveraged to create efficient, user-friendly applications. It serves as both a learning tool and a base for further development.
+
+## Prerequisites
+This is an example of how to list things you need to use the software and how to install them.
+- npm
+  \`\`\`sh
+  npm install npm@latest -g
+
+
+### Installation
+
+1. Clone the repo
+   \`\`\`sh
+   git clone https://github.com/AnishMane/MERN-Project.git
+   \`\`\`
+2. Install NPM packages
+   \`\`\`sh
+   npm install
+   \`\`\`
+3. Start the client using
+   \`\`\`sh
+   npm start
+   \`\`\`
+4. Start sever using
+   \`\`\`sh
+   npm run dev
+   \`\`\`
+
+
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch
+3. Commit your Changes
+4. Push to the Branch
+5. Open a Pull Request
+
+
+
+## Contact
+
+Anish - anishmane70@gmail.com
+
+
+`,
+    updated_at: new Date(),
+  },
+  {
+    title: 'Blog-API-Project',
+    slug: 'Blog-API-Project',
+    description:
+      'Created a Blog API and used it to power my website, making it easy to publish and manage blog content.',
+    image: 'https://i.postimg.cc/Ssm6msKx/API-Blog-Hero-banner-2x.png',
+    stacks: JSON.stringify(['JavaScript', 'Bootstrap', 'CSS']),
+    is_featured: false,
+    is_show: true,
+    link_demo: '',
+    link_github: 'https://github.com/AnishMane/Blog-API-Project',
     content: 'Detailed content about Project Two.',
     updated_at: new Date(),
   },
+  // {
+  //   title: '123',
+  //   slug: '123',
+  //   description:
+  //     'This project aims to identify cancer biomarkers using a Python backend (Flask API) and a React frontend, incorporating machine learning capabilities for predictions.',
+  //   image: 'https://i.postimg.cc/JnvxQ8q3/07-Blog-Cancer-Cells-L.jpg',
+  //   stacks: JSON.stringify(['Next.js', 'Tailwind CSS']),
+  //   is_featured: true,
+  //   is_show: true,
+  //   link_demo: 'https://project-two-demo.com',
+  //   link_github: 'https://github.com/user/project-two',
+  //   content: 'Detailed content about Project Two.',
+  //   updated_at: new Date(),
+  // },
   // Add more projects as needed
 ];
